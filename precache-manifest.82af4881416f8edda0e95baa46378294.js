f14936254a8d4e939f60a44518a0f41c78bfb792
Mujahid Ali <59640759+mujahidbcs92@users.noreply.github.com>
2023-01-10 20:40:29 +0530

    Updates

---
+++ precache-manifest.82af4881416f8edda0e95baa46378294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f04c74ea0bdd1e19b2f3",
+    "url": "/expense-manager/static/css/main.1f2b3017.chunk.css"
+  },
+  {
+    "revision": "f04c74ea0bdd1e19b2f3",
+    "url": "/expense-manager/static/js/main.f04c74ea.chunk.js"
+  },
+  {
+    "revision": "96e4d67c825c041d37b5",
+    "url": "/expense-manager/static/js/1.96e4d67c.chunk.js"
+  },
+  {
+    "revision": "23ba9fe62df962671e03",
+    "url": "/expense-manager/static/js/runtime~main.23ba9fe6.js"
+  },
+  {
+    "revision": "d660e54181320ee2a632a16e235a16c0",
+    "url": "/expense-manager/index.html"
+  }
+];
